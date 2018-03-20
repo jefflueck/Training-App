a5aa4c1b8bfbbbccb0b6e37e16013859b421102f
Jeff Lueck <lueckcfd@yahoo.com>
2018-03-20 12:11:14 -0500

    moved store into app.js

--- src/app.js
+++ src/app.js
@@ -2,5 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import { createStore } from 'redux';
+
+const store = createStore((state = { name: 'Jeff', station: 19 }) => state);
+
+console.log(store.getState());
+
 
 ReactDOM.render(<div><h1>Training Application</h1><p>This is the boilerplate for the training application.</p></div>, document.getElementById('app'));
